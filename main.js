6d4d96973c5f41d45304e18b2fd7f453919c771e
Fernando Cervantes <fer2311cervantes@gmail.com>
2024-03-19 12:15:29 -0600

    add: json file

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ import { getUsersUsingFetch, getUsersUsingAxios } from './src/js/getUser'
 const getUsers = async() => {
   //const users = await getUsersUsingFetch("https://reqres.in/api/users?page=2");
   const users = await getUsersUsingAxios("https://reqres.in/api/users?page=2");
+  //const users = await getUsersUsingAxios("./public/json/user.json");
   console.log( users );
   userCards(users);
 }
